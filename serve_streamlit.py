377d42429669430a477fffef4e5015d3fb3387ee
Lukas Mosser <lukas.mosser@gmail.com>
2024-07-21 11:38:19 +0000

    file formatting

--- serve_streamlit.py
+++ serve_streamlit.py
@@ -13,11 +13,29 @@ import modal
 # The `app.py` script imports three third-party packages, so we include these in the example's
 # image definition.
 
-hf_hub_download(repo_id="porestar/icml2024_embeddings", filename="icml_sessions.jsonl", local_dir="./data", repo_type="dataset")
+hf_hub_download(
+    repo_id="porestar/icml2024_embeddings",
+    filename="icml_sessions.jsonl",
+    local_dir="./data",
+    repo_type="dataset",
+)
 
-image = modal.Image.debian_slim(python_version="3.11").pip_install(
-    "streamlit~=1.35.0", "numpy~=1.26.4", "pandas~=2.2.2", "huggingface_hub", "lancedb", "openai", "tantivy", "jsonlines", "cohere", "streamlit_js_eval"
-).copy_local_dir("icml_finder", "/root/icml_finder")
+image = (
+    modal.Image.debian_slim(python_version="3.11")
+    .pip_install(
+        "streamlit~=1.35.0",
+        "numpy~=1.26.4",
+        "pandas~=2.2.2",
+        "huggingface_hub",
+        "lancedb",
+        "openai",
+        "tantivy",
+        "jsonlines",
+        "cohere",
+        "streamlit_js_eval",
+    )
+    .copy_local_dir("icml_finder", "/root/icml_finder")
+)
 
 app = modal.App(name="icml-finder", image=image)
 
@@ -48,8 +66,11 @@ streamlit_script_mount = modal.Mount.from_local_file(
 @app.function(
     allow_concurrent_inputs=100,
     mounts=[streamlit_script_mount],
-    secrets=[modal.Secret.from_name("icml-finder-openai"), modal.Secret.from_name("cohere-api-key")],
-    volumes={"/icml_data": modal.Volume.from_name("icml_data")}
+    secrets=[
+        modal.Secret.from_name("icml-finder-openai"),
+        modal.Secret.from_name("cohere-api-key"),
+    ],
+    volumes={"/icml_data": modal.Volume.from_name("icml_data")},
 )
 @modal.web_server(8000)
 def run():
